b458ba43247b28f446c6f2a6a0e5abb57fdb2d6f
Dustin Schau <dustinschau@gmail.com>
2020-03-21 18:29:16 -0700

    chore: fix seo

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -2,16 +2,43 @@ import React from 'react'
 import { Styled } from 'theme-ui'
 import { Global } from '@emotion/core'
 import { SkipNavLink } from '@reach/skip-nav'
+import { graphql, useStaticQuery } from 'gatsby'
 
 import { AuthProvider, AuthenticatedRoute } from '../components/authentication'
 import Navigation from '../components/navigation'
+import SEO from '../components/seo'
 // import Banner from '../components/banner'
 
 import '@reach/skip-nav/styles.css'
 
-export default ({ children }) => {
+const Layout = ({ children }) => {
+  const data = useStaticQuery(graphql`
+    {
+      contentfulAsset(title: { eq: "Alcorns Family on Cruise Ship" }) {
+        localFile {
+          childImageSharp {
+            resize(width: 1200) {
+              src
+              height
+              width
+            }
+          }
+        }
+      }
+
+      contentfulPage(slug: { eq: "/" }) {
+        title
+        description
+      }
+    }
+  `)
   return (
     <AuthProvider>
+      <SEO
+        {...data.contentfulPage}
+        image={data.contentfulAsset.localFile.childImageSharp.resize}
+      />
+
       <Styled.root>
         <Global
           styles={{
@@ -33,3 +60,5 @@ export default ({ children }) => {
     </AuthProvider>
   )
 }
+
+export default Layout
